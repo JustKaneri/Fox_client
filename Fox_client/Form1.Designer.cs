fce33016c2ba79edc93e733610024131ca436f2a
Your Name <you@example.com>
2021-02-21 15:54:43 +0300

    Add project files.

---
+++ Fox_client/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Fox_client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PbxMain = new System.Windows.Forms.PictureBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.PbxMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PbxMain
+            // 
+            this.PbxMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PbxMain.Image = ((System.Drawing.Image)(resources.GetObject("PbxMain.Image")));
+            this.PbxMain.Location = new System.Drawing.Point(0, 0);
+            this.PbxMain.Name = "PbxMain";
+            this.PbxMain.Size = new System.Drawing.Size(834, 474);
+            this.PbxMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PbxMain.TabIndex = 0;
+            this.PbxMain.TabStop = false;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Fox_клиент";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(97)))), ((int)(((byte)(118)))));
+            this.ClientSize = new System.Drawing.Size(834, 474);
+            this.Controls.Add(this.PbxMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fox_клиент";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PbxMain)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PbxMain;
+        public System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Fox_client/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net.Sockets;
+using System.Threading;
+using DLl_Profile;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Net;
+using Microsoft.Win32;
+
+namespace Fox_client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Клиент.
+        /// </summary>
+        private TcpClient client;
+        private string IpAdress = "127.0.0.1";
+        private int Port = 9000;
+        /// <summary>
+        /// Поток подключения
+        /// </summary>
+        private Thread th1;
+        private Thread th2;
+
+        private Info ByteToInfo(byte[] buff)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+
+            MemoryStream ms = new MemoryStream(buff);
+
+            return (Info)bf.Deserialize(ms);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //using (var reg = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run",true))
+            //{
+            //    if (reg.GetValue("Shark_Client") == null)
+            //    {
+            //        string exepath = Application.ExecutablePath;
+            //        reg.SetValue("Shark_Client", "\"" + exepath + "\" min");
+            //    }
+            //}
+
+            this.Hide();
+            this.ShowInTaskbar = false;
+            WindowState = FormWindowState.Minimized;
+
+            ReadFromFile();
+            th2 = new Thread(Connect);
+            th2.Start();
+
+        }
+
+        private void ReadFromFile()
+        {
+            string path = Path.GetDirectoryName(Application.ExecutablePath) + "/Foregin.key";
+
+            if (File.Exists(path))
+                using (BinaryReader br = new BinaryReader(File.Open(path, FileMode.Open)))
+                {
+                    IpAdress = br.ReadString();
+                    Port = int.Parse(br.ReadString());
+                }
+        }
+
+        private void SetImage(Image image)
+        {
+            PbxMain.Image = image;
+        }
+
+        private void GetMessage()
+        {
+            NetworkStream stream = null;
+            try
+            {
+               
+                stream = client.GetStream();
+                while (true)
+                {
+
+                    byte[] byff = new byte[20000000];
+
+                    try
+                    {
+                        stream.Read(byff, 0, byff.Length);
+                    }
+                    catch (Exception e)
+                    {
+                        //MessageBox.Show(e.ToString());
+                        continue;
+                    }
+
+
+
+                    Info inf;
+                    inf = ByteToInfo(byff);
+
+
+                    if (inf.Command == "Start")
+                    {
+                        SetImage(inf.Screen);
+                        if (ShowInTaskbar == false)
+                        {
+                            notifyIcon1.Text = "Fox_клиент\nСтатус: идет демонстрация экрана";
+                            ShowForm();
+
+                        }
+
+                    }
+                    else
+                    {
+                        if (ShowInTaskbar)
+                        {
+                            notifyIcon1.Text = "Fox_клиент\nСтатус: соединение установлено";
+                            HideForm();
+                        }
+
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                stream?.Close();
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            client?.Close();
+            th1?.Abort();
+            th2?.Abort();
+        }
+
+        private void HideForm()
+        {
+            Invoke(new MethodInvoker(delegate
+            {
+                //this.Hide();
+                this.WindowState = FormWindowState.Minimized;
+                this.ShowInTaskbar = false;
+
+            }));
+      
+           
+        }
+
+        private void ShowForm()
+        {
+            Invoke(new MethodInvoker(delegate
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+                this.ShowInTaskbar = true;
+
+            }));
+        }
+
+        private void Connect()
+        {
+            while (true)
+            {
+                try
+                {
+                    client = new TcpClient(IpAdress, Port);
+                    th1 = new Thread(GetMessage);
+
+                    notifyIcon1.Text = "Fox_клиент\nСтатус: соединение установленно";
+                    th1.Start();
+                    th1.Join();
+
+                    notifyIcon1.Text = "Fox_клиент\nСтатус: соединение разоравнно";
+                    PbxMain.Image = Properties.Resources.resize;
+                    HideForm();
+
+                }
+                catch
+                {
+
+                }
+                finally
+                {
+                    client?.Close();
+                    th1?.Abort();
+                }
+
+                Thread.Sleep(1000);
+            }
+
+        }
+
+    }
+}
